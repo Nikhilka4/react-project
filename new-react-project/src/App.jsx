af35cdffce1cfc3d8434ba063369516da235031c
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-02-21 13:53:32 +0530

    initial

---
+++ new-react-project/src/App.jsx
@@ -0,0 +1,22 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar"
+
+function App() {
+  return (
+    <div>
+      <section>
+        <Navbar />
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Portfolio4Cards</section>
+      <section>contact</section>
+    </div>
+  )
+}
+
+export default App
---
+++ new-react-project/src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./navbar.scss"
+
+function Navbar() {
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+            <span>Lama Dev</span>
+            <div className="social">
+                <a href="#"><img src="/facebook.png" alt="" /></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
